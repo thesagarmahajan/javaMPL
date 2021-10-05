8d25c4116a105dffc935e08d1912d319c9b6a38a
thesagarmahajan <sagarmahajan31997@gmail.com>
2021-10-05 17:51:00 +0530

    Multithreading Example

--- MultiThreading/MultiThreadingExample.java
+++ MultiThreading/MultiThreadingExample.java
@@ -2,10 +2,25 @@ package MultiThreading;
 
 public class MultiThreadingExample {
     public static void main(String[] args) {
-        ThreadDemo1 t1 = new ThreadDemo1("ThreadNo-1111");
-        ThreadDemo1 t2 = new ThreadDemo1("ThreadNo-2222");
+        ThreadDemo1 t1 = new ThreadDemo1("ThreadNo-1111", 1, 25000);
+        ThreadDemo1 t2 = new ThreadDemo1("ThreadNo-2222", 25001, 50000);
         t1.start();
         t2.start();
+
+        long startTime = System.currentTimeMillis();
+
+        try {
+            t1.join();  
+            t2.join();  
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        
+        System.out.println(ThreadDemo1.getPrimes());
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        System.out.println("TotalTime: "+elapsedTime+" Milliseconds");
+
     }
 
     /* 
--- MultiThreading/ThreadDemo1.java
+++ MultiThreading/ThreadDemo1.java
@@ -1,18 +1,37 @@
 package MultiThreading;
+import java.util.ArrayList;
 
 public class ThreadDemo1 extends Thread{
     
     private String threadName;
+    private int st, ed;
+    private static ArrayList<Integer> primes = new ArrayList<>();
 
-    public ThreadDemo1(String threadName) {
+    public ThreadDemo1(String threadName, int start, int end) {
         super(threadName);
+        this.st = start;
+        this.ed = end;
         this.threadName = threadName;
     }
 
+    public static ArrayList<Integer> getPrimes(){
+        return ThreadDemo1.primes;
+    }
+
     public void run(){
-        for(int i=1;i<=5;i++){
-            System.out.println(i+" ( "+this.threadName+" )");
-        }
+
+        for(int i = this.st; i <= this.ed; i++) {
+            int count =0;
+            for(int j=1;j<=i;j++){
+                if(i%j==0){
+                    count++;
+                }
+            }
+            if(count==2){
+                this.primes.add(i);
+            }
+         }
+
     }
 
 }
